c956b5b53ba76d66e144e6d09ef08dae7e1bb3f7
joshuapregua <joshua.pregua@wpengine.com>
2025-12-01 14:44:22 +0800

    feat: add ci pipelines for linting, building and unit tests

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import vue from 'eslint-plugin-vue';
+import vueParser from 'vue-eslint-parser';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+        languageOptions: {
+        parser: typescriptParser,
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        },
+        plugins: {
+        '@typescript-eslint': typescript,
+        },
+        rules: {
+        ...typescript.configs.recommended.rules,
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+        parser: vueParser,
+        parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            parser: typescriptParser,
+        },
+        },
+        plugins: {
+        vue,
+        },
+        rules: {
+        ...vue.configs['vue3-recommended'].rules,
+        'vue/multi-word-component-names': 'off',
+        },
+    },
+    {
+        ignores: [
+        'node_modules/**',
+        'dist/**',
+        'build/**',
+        '*.js',
+        '!eslint.config.js',
+        ],
+    },
+];
