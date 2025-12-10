962c0f99af3eeeac350ecb6ee1a17c85392fa5fb
Rcasas14 <reymartcasas123@gmail.com>
2025-12-10 11:57:42 +0800

    CI test

--- src/renderer/components/HelloWorld.vue
+++ src/renderer/components/HelloWorld.vue
@@ -24,6 +24,7 @@ const count = ref(0)
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <p class="test">test</p>
 </template>
 
 <style scoped>
